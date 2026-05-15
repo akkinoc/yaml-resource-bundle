48a2ee0a7e686242c10afedd005d852de3682730
Akihiro Kondo <akkinoc@gmail.com>
2026-05-16 01:09:05 +0900

    Add gradle plugin: org.gradle.toolchains.foojay-resolver-convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,15 @@
-rootProject.name = "yaml-resource-bundle"
-
-include("yaml-resource-bundle")
-include("examples:simple-java")
-include("examples:simple-kotlin")
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
 }
+
+rootProject.name = "yaml-resource-bundle"
+
+include("yaml-resource-bundle")
+include("examples:simple-java")
+include("examples:simple-kotlin")
